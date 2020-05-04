4848aacc3064152671ee151a8fbb88d261b857f5
unknown <thanmay4@gmail.com>
2020-05-04 15:05:44 +0530

    Updates

---
+++ precache-manifest.cb278576dff1cb3a1149d73f075ac5e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e0cda5bde116493ed588a4e2b63e8bf",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "ba53522c0ee2d7873751",
+    "url": "/covid19/static/css/main.80f132b8.chunk.css"
+  },
+  {
+    "revision": "22ccabeaa5babc40f2fe",
+    "url": "/covid19/static/js/2.46b98573.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19/static/js/2.46b98573.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba53522c0ee2d7873751",
+    "url": "/covid19/static/js/main.56e92a7f.chunk.js"
+  },
+  {
+    "revision": "096422a7c9ef5cf0523f",
+    "url": "/covid19/static/js/runtime-main.076d3c7a.js"
+  }
+]);
